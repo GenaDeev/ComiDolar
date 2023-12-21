fec854e06e826caff409b8373f934a8b56115673
GenaAaaj <71671650+GenaAaaj@users.noreply.github.com>
2023-12-21 15:12:43 -0300

    Update main.js

--- tarjeta/assets/js/main.js
+++ tarjeta/assets/js/main.js
@@ -25,50 +25,30 @@ async function calculate() {
     // Resto de tu lógica de cálculo
     const price = parseFloat(priceInput.value);
     const pesos = price * oficial;
-    let reg = 0;
-    if (provInput == 'none') {
-      var reg = 0;
+
+    let reg = 0;  // Inicializa reg aquí, después de obtener pesos
+
+    if (provInput.value === 'caba' || provInput.value === 'pba') {
+      reg = 0.02 * pesos;
+    } else if (provInput.value === 'cdba' || provInput.value === 'ttff') {
+      reg = 0.03 * pesos;
+    } else if (provInput.value === 'lppa') {
+      reg = 0.01 * pesos;
+    } else if (provInput.value === 'rrnn') {
+      reg = 0.05 * pesos;
+    } else if (provInput.value === 'salt') {
+      reg = 0.036 * pesos;
+    } else if (provInput.value === 'chac') {
+      reg = 0.055 * pesos;
+    } else if (provInput.value === 'neuq') {
+      reg = 0.04 * pesos;
     }
-    else {
-      if (provInput.value == 'caba' || provInput == 'pba') {
-        var reg = 0.02 * pesos;
-      }
-      else {
-        if (provInput.value == 'cdba' || provInput == 'ttff') {
-          var reg = 0.03 * pesos;
-        }
-        else {
-          if (provInput.value == 'lppa') {
-            var reg = 0.01 * pesos;
-          }
-          else {
-            if (provInput.value == 'rrnn') {
-              var reg = 0.05 * pesos;
-            }
-            else {
-              if (provInput.value == 'salt') {
-                var reg = 0.036 * pesos;
-              }
-              else {
-                if (provInput.value == 'chac') {
-                  var reg = 0.055 * pesos;
-                }
-                else {
-                  if (provInput.value == 'neuq') {
-                    var reg = 0.04 * pesos;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-    console.log(reg);
+
     // Resto de tu lógica de cálculo
     const impais = 0.3 * pesos;
     const gan = 0.3 * pesos;
     const total = pesos + impais + gan + reg;
+
     // Actualizar elementos HTML
     document.getElementById('pesosResult').value = pesos.toFixed(2);
     document.getElementById('impaisResult').value = impais.toFixed(2);
@@ -79,6 +59,7 @@ async function calculate() {
     console.error('Error:', error);
   }
 }
+
 document.addEventListener("DOMContentLoaded", function () {
   var links = document.querySelectorAll('a[href^="#"]');
 
