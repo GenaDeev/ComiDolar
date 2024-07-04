1acea73273adcac6f054fa2d47118fa8e1e631db
GenaAaaj <genaroelgenio@hotmail.com>
2024-07-04 18:48:53 -0300

    Upgraded site and web application using Astro & TailwindCSS. New v.3.1.00(BETA)

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
