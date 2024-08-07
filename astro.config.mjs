8a66af2b3d8484ce8e05e230c6be7c3c003c932b
GenaAaaj <genaroelgenio@hotmail.com>
2024-08-07 20:56:01 -0300

    Deleted Vercel Web Analytics 📐

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  output: "static"
 });
