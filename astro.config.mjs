a902f0fa5b88726da02394d82e820b45e806fb2c
GenaDeev <genaroelgenio@hotmail.com>
2024-09-18 21:26:53 -0300

    📊🗑️Deleted Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(), 
-    partytown({
-        config: {
-          forward: ["dataLayer.push"],
-        },
-    }),
+    tailwind()
 ],
   output: "static"
 });
