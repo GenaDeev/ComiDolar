9e736787012d2c4f16f17a48a5b8cb9e2c267018
GenaDeev <genaroelgenio@hotmail.com>
2024-10-31 21:06:54 -0300

    ℹ️ Added astro sitemap integration (not used yet)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({
-    experimentalReactChildren: true,
-  }),],
-  output: "static"
+  site: "https://comidolar.com.ar",
+  integrations: [
+    tailwind(),
+    react({
+      experimentalReactChildren: true,
+    }),
+  ],
+  output: "static",
 });
