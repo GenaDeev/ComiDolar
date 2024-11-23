c9c0d8c59c0db27d8f49414cc9971f630b96a375
GenaDeev <genaroelgenio@hotmail.com>
2024-11-23 00:04:18 -0300

    ✅🛜 Added RSS to blog posts

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,26 @@
+import rss from '@astrojs/rss';
+import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Blog de ComiDólar',
+    description: 'Todas las últimas publicaciones de ComiDólar a través de su blog oficial. Noticias del dólar, del CNBA y mucho más.',
+    site: context.site,
+    items: blog.map((post) => {
+      const contentHtml = sanitizeHtml(parser.render(post.body));
+      return {
+        title: post.data.title,
+        pubDate: post.data.date,
+        description: post.data.description,
+        categories: post.data.tags,
+        author: post.data.author,
+        link: `/blog/${post.slug}/`,
+        content: contentHtml,
+      };
+    }),
+  });
+}
