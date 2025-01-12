6932178b7953be66589a5a2ce7a120fe968855b0
GenaDeev <genaroelgenio@hotmail.com>
2025-01-11 21:58:53 -0300

    ✅ feat(optimization): replaced carousel component with a lighter one

--- src/components/CarouselElement.tsx
+++ src/components/CarouselElement.tsx
@@ -1,25 +1,109 @@
-import { Carousel } from "flowbite-react";
+import { useCallback } from "react";
+import type { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
 
-export default function CarouselElement() {
-    const images: string[] = [
-        "https://comidolar.com.ar/assets/img/wide-c.webp",
-        "https://comidolar.com.ar/assets/img/colegio.webp",
-        "https://comidolar.com.ar/assets/img/mundialbanner.webp",
-        "https://comidolar.com.ar/assets/img/colegio2.webp"
-    ]
+const CarouselElement: React.FC = () => {
+  const options: EmblaOptionsType = { loop: true };
+  const slides: string[] = [
+    "https://comidolar.com.ar/assets/img/wide-c.webp",
+    "https://comidolar.com.ar/assets/img/colegio.webp",
+    "https://comidolar.com.ar/assets/img/mundialbanner.webp",
+    "https://comidolar.com.ar/assets/img/colegio2.webp",
+  ];
 
-    return (
-        <Carousel className="bg-zinc-300 dark:bg-darkless rounded-xl p-2 fb-carousel">
-            {
-                images.map(
-                    image =>
-                        <img
-                            key={'Imagen del carousel: ' + image.split('/')[3].split('.')[0]}
-                            className="carousel-image object-cover h-full"
-                            src={image} alt="Imagen del Carousel de ComiDolar">
-                        </img>
-                )
-            }
-        </Carousel>
-    )
-}
+  // Initialize Embla carousel with autoplay
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (autoplay) {
+      const action =
+        autoplay.options.stopOnInteraction === false
+          ? autoplay.reset
+          : autoplay.stop;
+      action();
+    }
+  }, []);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  );
+
+  return (
+    <section
+      className="embla"
+      style={{
+        width: "100%",
+        height: "200px",
+        marginBottom: "8px",
+        position: "relative",
+      }}
+    >
+      <div
+        className="embla__viewport"
+        style={{ overflow: "hidden", width: "100%", height: "100%" }}
+        ref={emblaRef}
+      >
+        <div
+          className="embla__container"
+          style={{
+            display: "flex",
+            transition: "transform 0.3s ease",
+            height: "100%",
+          }}
+        >
+          {slides.map((slide, index) => (
+            <div
+              className="embla__slide"
+              style={{ position: "relative", flex: "0 0 100%" }}
+              key={`slide-${index}`}
+            >
+              <img
+                className="w-full h-full object-cover bg-zinc-200 dark:bg-darkless rounded-xl"
+                src={slide}
+                alt={`Imagen número ${index + 1} del carrusel`}
+                loading="lazy"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className="embla__controls"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "1rem",
+          justifySelf: "end",
+        }}
+      >
+        <div className="embla__dots" style={{ display: "flex", gap: "0.5rem" }}>
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={`dot-${index}`}
+              onClick={() => onDotButtonClick(index)}
+              className={`embla__dot border-2 ${
+                index === selectedIndex
+                  ? "embla__dot--selected border-green-500"
+                  : " border-zinc-200 dark:border-darkless"
+              }`}
+              style={{
+                width: "20px",
+                height: "20px",
+                borderRadius: "50%",
+                cursor: "pointer",
+              }}
+              aria-label={`Ir al slide ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CarouselElement;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,8 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'media',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		flowbite.content(),
 	],
 	theme: {
 		extend: {
@@ -20,8 +17,4 @@ export default {
 			}
 		},
 	},
-	plugins: [
-		require('flowbite/plugin'),
-		flowbite.plugin(),
-	],
 }
