a27cbcb70b9e50a974a1e5019df5a1f0d2220415
GenaAaaj <genaroelgenio@hotmail.com>
2025-03-07 12:16:13 -0300

    📎feat(routing): added @ route alias

--- src/components/DonationTable.tsx
+++ src/components/DonationTable.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 export default function DonationGrid() {
     const [donationTable, setDonationTable] = useState([]);
     const [visibleCount, setVisibleCount] = useState(7);
